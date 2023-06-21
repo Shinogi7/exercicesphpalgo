23977d305c427470e8bf6ca7ccf4625eef0467ad
Shinogi7 <thomas.giresse@viacesi.fr>
2023-06-21 21:43:43 +0200

    exercice 1

---
+++ exercice.php
@@ -0,0 +1,15 @@
+<?php
+echo"Exercice 1 : Écrivez un programme PHP qui affiche tous les nombres pairs de 1 à 50.";
+echo "<br>";
+
+
+for ($i = 1;$i <= 50; $i++) {
+
+    if($i%2==0) 
+    echo "$i<br>";
+    
+    
+} ;
+
+
+?>
