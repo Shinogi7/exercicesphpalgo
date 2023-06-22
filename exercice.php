806f4c7b2d7f70a78e92d920afbfe9f74bc1f4e6
Shinogi7 <thomas.giresse@viacesi.fr>
2023-06-22 21:53:05 +0200

    exercice 3

--- exercice.php
+++ exercice.php
@@ -1,5 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercixes</title>
+</head>
+<body>
 <?php
-echo"Exercice 1 : Écrivez un programme PHP qui affiche tous les nombres pairs de 1 à 50.<br>";
+echo"Exercice 1 : Écrivez un programme PHP qui affiche tous les nombres pairs de 1 à 50.<br><br>";
 
 
 for ($i = 1;$i <= 50; $i++) {
@@ -9,9 +17,7 @@ for ($i = 1;$i <= 50; $i++) {
     };
 
 
-echo "<br> Exercice 2 : Écrivez un programme PHP qui prend en entrée un tableau de nombres et renvoie la somme de tous 
-les éléments du tableau.
-<br>";
+echo "<br> Exercice 2 : Écrivez un programme PHP qui prend en entrée un tableau de nombres et renvoie la somme de tous les éléments du tableau. <br>";
 
 $liste_nombre = array(1,5,4,6,7,2);
 
@@ -20,5 +26,25 @@ $somme = array_sum($liste_nombre);
 echo "<br> $somme <br>";
 
 
+echo "<br> Exercice 3 : Écrivez un programme PHP qui prend en entrée une chaîne de caractères et renvoie la longueur de cette chaîne. <br>"
+
+//Input de la chaine de caractère en html, 
+
+?> 
+<form action="exercice.php" method="get">
+Entrez une chaine de caractère : <Input type="text" name="chaine">
+</form><?php
+
+$chaine = $_GET["chaine"];
+
+$longchaine = strlen($chaine);
+
+
+echo("$chaine = $longchaine");
+
+
 
 ?>
+
+</body>
+</html>
